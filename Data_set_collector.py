649fb87f546ae0ab014effb6809d2d825208dd4a
im-minion <vaibhavminiyar@gmail.com>
2017-08-21 17:34:55 +0530

    AuthSignupDone

--- Data_set_collector.py
+++ Data_set_collector.py
@@ -1,9 +1,12 @@
+import json
+import urllib
+
 from flask import Flask, render_template , request
 from firebase import firebase
 
 app = Flask(__name__)
 firebasevar = firebase.FirebaseApplication('https://datasetcollector-b1daa.firebaseio.com/')
-
+firebase_apikey = "AIzaSyDmbVrxMd2l1Pq18zTvquLUlgBCIPErqqY"
 @app.route('/')
 def Base_qstn_paper_set():
     return render_template('AuthThings.html')
@@ -11,9 +14,11 @@ def Base_qstn_paper_set():
 @app.route('/auth',methods=['POST','GET'])
 def auth():
     if request.method == 'POST':
-        fname = request.form['fname']
-        lname = request.form['lname']
-        print(fname," ",lname)
+        email = request.form['emailId']
+        password = request.form['password']
+        print(email," ",password)
+        register(email, password)
+        # auth_with_password(fName,lName)
     return render_template('first.html')
 
 @app.route('/foo',methods=['POST','GET'])
@@ -34,6 +39,32 @@ def foo():
 #     request_ref = 'https://auth.firebase.com/auth/firebase?firebase={0}&email={1}&password={2}'.\
 #         format(self.fire_base_name, email, password)
 #     request_object = self.requests.get(request_ref)
+#     print(request_object)
 #     return request_object.json()
+
+def register(email, password):
+
+    my_data = dict()
+    my_data["email"] = email
+    my_data["password"] = password
+    my_data["returnSecureToken"] = True
+
+    print("a")
+
+    json_data = json.dumps(my_data).encode()
+    headers = {"Content-Type": "application/json"}
+    request = urllib.request.Request("https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key="+firebase_apikey, data=json_data, headers=headers)
+
+    print("b")
+
+    try:
+        loader = urllib.request.urlopen(request)
+        print("c")
+
+    except urllib.error.URLError as e:
+        message = json.loads(e.read())
+        print(message["error"]["message"])
+    else:
+        print(loader.read())
 if __name__ == '__main__':
     app.run()
