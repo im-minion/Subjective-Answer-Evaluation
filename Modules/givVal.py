ebe2646df89a2d6e8cea33333d2f807002a6b2cd
im-minion <vaibhavminiyar@gmail.com>
2018-01-27 20:49:01 +0530

    modules added

---
+++ Modules/givVal.py
@@ -0,0 +1,81 @@
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
+import nav_test
+import pyrebase
+import math
+import json
+import requests
+
+model_answer = 'Encapsulation is an object-oriented programming concept that binds together the data and functions that manipulate the data, and that keeps both safe from outside interference and misuse.Data encapsulation led to the important OOP concept of data hiding. If a class does not allow calling code to access internal object data and permits access through methods only, this is a strong form of abstraction or information hiding known as encapsulation. Data encapsulation is a mechanism of bundling the data, and the functions that use them and data abstraction is a mechanism of exposing only the interfaces and hiding the implementation details from the user. Abstraction and encapsulation are complementary concepts: abstraction focuses on the observable behavior of an object. encapsulation focuses upon the implementation that gives rise to this behavior. encapsulation is most often achieved through information hiding, which is the process of hiding all of the secrets of object that do not contribute to its essential characteristics.  Encapsulation is the process of combining data and functions into a single unit called class. In Encapsulation, the data is not accessed directly; it is accessed through the functions present inside the class. In simpler words, attributes of the class are kept private and public getter and setter methods are provided to manipulate these attributes. Thus, encapsulation makes the concept of data hiding possible Abstraction is a process where you show only “relevant” data and “hide” unnecessary details of an object from the user.'
+
+answer1 = 'It is object oreinted concept related to the data hiding. Abstraction of the program is the encapsulation. It shows the relvant data. It is the mechanism of binding the data, and the function that use them.'
+
+answer2 = 'Encapsulation is hiding data. It can be visualised as an Abstract view of the program. '
+
+keywords = ['binds', 'together', 'relevant data', 'data hiding', 'data hiding', 'abstraction', 'combining data']
+
+'''
+e = 1
+vg = 2
+g = 3
+o = 4
+p = 5
+vp = 6
+
+Grammar:
+y = 1
+n = 0
+'''
+
+
+# KEYWORDS =>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+count = 0
+for i in range(len(keywords)):
+	if keywords[i] in answer1:
+		# print (keywords[i])
+		count = count + 1
+k = 0
+if(count==len(keywords)):
+	k = 1
+elif( count== (len(keywords)-1)):
+	k = 2
+elif (count==(len(keywords)-2)):
+	k = 3
+elif (count==(len(keywords)-3)):
+	k = 4
+elif (count==(len(keywords)-4)):
+	k = 5
+elif (count==(len(keywords)-5)):
+	k = 6
+
+# GRAMMAR =>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+req = requests.get("https://api.textgears.com/check.php?text="+answer1+"&key=JmcxHCCPZ7jfXLF6")
+no_of_errors = len(req.json()['errors'] )
+
+if no_of_errors > 5:
+	g = 0
+else:
+	g = 1
+
+# QST =>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+print("fuzz1 ratio: ",fuzz.ratio(model_answer,answer1))
+q = math.ceil(fuzz.token_set_ratio(model_answer,answer1) * 6 / 100)
+
+
+
+print("Keywords : ",k)
+print("Grammar : ",g)
+print("Qusestion Specific Things : ",q)
+
+nav_test.predict(k,g,q)
+
+
+# print("fuzzz2 : ",fuzz.token_set_ratio(model_answer,answer2))
+
+
+# qst1 = answer1.split('Example' or 'eg' or 'example')
+
+# print(qst1[1])
---
+++ Modules/nav_test.py
@@ -0,0 +1,94 @@
+from sklearn.naive_bayes import GaussianNB
+import numpy as np
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('finaldataset.csv')
+xf = df[['keyword','grammar','qst']]
+# intigrate keyword, grammar, qst :)
+''''
+keywords and qst:
+e = 1
+vg = 2
+g = 3
+o = 4
+p = 5
+vp = 6
+
+Grammar:
+y = 1
+n = 0
+
+class labels 0.1 to 0.9 simplifies to 0 to 9 for calculation purpose
+'''
+
+x = np.array(xf.values)
+yf = df[['class']]
+y = np.array(yf.values).ravel()
+clf = GaussianNB()
+clf.fit(x,y)
+
+def predict(k, g, q):
+	predicted = clf.predict([[k,g,q]])
+	accuracy = clf.predict_proba([[k,g,q]])
+	print(predicted)
+	print(accuracy)
+	print(np.max(accuracy))
+
+# predict(2,0,4)
+# predict for 
+# 1. keyword = verygood(2), grammar = no(0), qst = ok(4) 
+# => 6
+# 2. keyword = verygood(2), grammar = no(0), qst = vg (2)
+# => 8
+
+
+# print(yf.values)
+# x= np.array([[-3,7],[1,5], [1,2], [-2,0], [2,3], [-4,0], [-1,1], [1,1], [-2,2], [2,7], [-4,1], [-2,7]])
+# y = np.array([3, 3, 3, 3, 4, 3, 3, 4, 3, 4, 4, 4])
+# # lx= np.array(
+# # 	[
+# # 		['e','y','e'],
+# # 		['e','n','e'],
+# # 		['vg','n','e'],
+# # 		['e','y','vg'],
+# # 		['e','n','g']
+# # 	]
+# # 	)
+# # ly = np.array(
+# # 	[
+# # 		0.9,
+# # 		0.9,
+# # 		0.8,
+# # 		0.8,
+# # 		0.7
+# # 	]
+# # 	)
+# print(yf.values)
+# x= np.array([[-3,7],[1,5], [1,2], [-2,0], [2,3], [-4,0], [-1,1], [1,1], [-2,2], [2,7], [-4,1], [-2,7]])
+# y = np.array([3, 3, 3, 3, 4, 3, 3, 4, 3, 4, 4, 4])
+# # lx= np.array(
+# # 	[
+# # 		['e','y','e'],
+# # 		['e','n','e'],
+# # 		['vg','n','e'],
+# # 		['e','y','vg'],
+# # 		['e','n','g']
+# # 	]
+# # 	)
+# # ly = np.array(
+# # 	[
+# # 		0.9,
+# # 		0.9,
+# # 		0.8,
+# # 		0.8,
+# # 		0.7
+# # 	]
+# # 	)
+# model = GaussianNB()
+
+# # model.fit(lx,ly)
+# model.fit(x,y)
+
+# predicted = model.predict([[-1,-3],[-3,2]])
+# print(predicted)
